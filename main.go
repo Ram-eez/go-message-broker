ab059f6812b0293e9b6f41ed47d9642e9a60c6b6
Ram-eez <rameez2838@gmail.com>
2024-11-06 13:34:49 +0530

    updated main.go

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go-message-broker/broker"
+	"time"
+)
 
 func main() {
-	fmt.Println("xyz")
+	broker := broker.NewBroker()
+	subscriber := broker.Subscribe("thenews")
+
+	go func() {
+		for {
+			select {
+			case msg, ok := <-subscriber.Channel:
+				if !ok {
+					fmt.Println("Subscriber Channel is closed")
+					return
+				}
+				fmt.Printf("Received the message: %v\n", msg)
+			case <-subscriber.Unsubscribe:
+				fmt.Println("Unsubscribed from the channel")
+				return
+			}
+		}
+	}()
+
+	broker.Publish("thenews", "hello world news")
+	broker.Publish("thenews", "doge is gay")
+
+	time.Sleep(time.Second)
+	// broker.Unsubscribe("thenews", subscriber)
+
+	broker.Publish("thenews", "doge is not gay")
+	time.Sleep(time.Second)
+
 }
